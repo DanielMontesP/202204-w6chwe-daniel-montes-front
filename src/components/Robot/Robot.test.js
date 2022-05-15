c06a4434a3621ee4aed90dc4413280a9e8c76072
Daniel Montes <daniel.montes@gmail.com>
2022-05-15 15:34:23 +0200

    Add test for robot

---
+++ src/components/Robot/Robot.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import RobotsProvider from "../../store/context/Robotsrovider";
+import Robot from "./Robot";
+
+describe("Given a component TvShowMobileDetail", () => {
+  describe("When it receives name=Stars Wars posterURL=url rating=10.0 year=2020 genre=Acción", () => {
+    test("Render component TvShowMobileDetailRender", () => {
+      const history = createMemoryHistory();
+
+      render(
+        <RobotsProvider>
+          <Router location={history.location} navigator={history}>
+            <Robot name="Robocop" robotURL="url" />
+          </Router>
+        </RobotsProvider>
+      );
+      const linkElement = screen.getByText("Robocop");
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
+});
